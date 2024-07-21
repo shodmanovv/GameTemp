9102b9d1aeeca8cd7aab9f6cfd1e850cce76a89b
shodmanovv <150368733+shodmanovv@users.noreply.github.com>
2024-07-21 18:24:22 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN="6377790366:AAFBLLRrPCHTAzvBzwdeIJH5pdBEC2HB6FA" #ТОКЕН БОТА. ВЗЯТЬ ТУТ @Bot_father
+owner_id=1455110797 #АЙДИ ДЛЯ АДМИНКИ
+log_group=-1001966197110 #АЙДИ ЧАТА С ЛОГАМИ
+orig_channel=-1002110924392 #АЙДИ КАНАЛА БОТА
+QIWI_TOKEN='eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFyc3QweS0wMCIsInVzZXJfaWQiOiI5OTY3MDU0NDkxMTEiLCJzZWNyZXQiOiIzMDMyNGQwMGEyMDgwOTA5NDlhMTk2OTIxNDJkNThlMGVkMmM5MDZhMGQ1MTVkYzIzOTliMTVhNGFiN2Y5OGE0In19'
+channell=-1002110924392 #АЙДИ КАНАЛА
+bot_name = "temp_game_bot" #ЮЗЕР БОТА ДЛЯ РЕФ ССЫЛКИ БЕЗ @
+
+start_money = 1000000 #СТАРТОВОЕ КОЛВО ДЕНЕГ
+cash_family = 5 #ЦЕНА СОЗДАНИЯ СЕМЬИ
+
+owner = '<a href="https://t.me/its_pulat">@admin</a>'
+channel = '<a href="https://t.me/TempBotDev">TEMP | DEV</a>' 
+chat = '<a href="https://t.me/temp_chatik">TEMP | CHAT</a>'
+chat_id= -1002122704489 #АЙДИ ЧАТА
